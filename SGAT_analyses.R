e4b50974894be64f1efcfc528bd3e36225ca0a40
Eli-S-Bridge <ebridge@ou.edu>
2016-08-08 22:41:52 +0000

    Tried to add group analysis to SGAT. But it fails.

--- SGAT_analyses.R
+++ SGAT_analyses.R
@@ -84,7 +84,6 @@ lines(1:100, dlnorm(1:100, fitml$estimate[1], fitml$estimate[2]), col = "firebri
 alpha <- c(fitml$estimate[1], fitml$estimate[2]) ## Twilight model parameters
 
 
-
 #SIMEON
 #Need some help here. The zenith angle needs to be about 97 to get reasonable results
 zenith0 <- median(z) #used only for simple threshold map
@@ -92,8 +91,9 @@ zenith0
 zenith  <- quantile(z, prob = .05)  #use the 95% of the zenith angles. Why??? This is too low a value
 zenith <- zenith0  #still too low a value
 zenith <- 97  #Maybe a bit too high. 
+zenith <- median(z)
 
-path <- thresholdPath(twl$Twilight, twl$Rise, zenith = zenith0, tol = 0.15)
+path <- thresholdPath(twl$Twilight, twl$Rise, zenith = zenith, tol = 0.15)
 #have a quick look to see that it makes sense.
 tripMap(path$x, equinox = TRUE, xlim = c(-90,-70), ylim = c(10,50), legend = TRUE)
 
@@ -192,7 +192,10 @@ fit <- estelleMetropolis(model,proposal.x,proposal.z,
                          z0=chainLast(fit$z),
                          iters=2000,thin=20,chains=2)
 
-
+#summarize the location data in fit
+s <- locationSummary(fit$x,time=model$time,collapse=T)
+#save(s, file = "data/SGAT_results1.Rdata")
+load(file = "data/SGAT_results1.Rdata")
 
 #Have a look at the results
 library(maptools)
@@ -202,8 +205,6 @@ data(wrld_simpl)
 xlim = c(-90,-70)
 ylim = c(10,50)
 
-
-s <- locationSummary(fit$x,time=model$time,collapse=T)
 fixedz <- fixedx[-length(fixedx)] > 0 & fixedx[-length(fixedx)]==fixedx[-1]
 dt <- ifelse(fixedz,0,model$dt)
 im <- locationImage(fit$z,xlim=xlim,ylim=ylim,nx=4*diff(xlim),ny=4*diff(ylim),
@@ -290,7 +291,7 @@ model <- thresholdModel(twl$Twilight,twl$Rise,
                         twilight.model="ModifiedLogNormal",
                         alpha=alpha,beta=beta,
                         logp.x = log.prior, logp.z = log.prior, #mask is applied here
-                        x0=x0,z0=z0,zenith=98.6,fixedx=fixedx)
+                        x0=x0,z0=z0,zenith=zenith,fixedx=fixedx)
 
 proposal.x <- mvnorm(S=diag(c(0.005,0.005)),n=nlocation(x0))
 proposal.z <- mvnorm(S=diag(c(0.005,0.005)),n=nlocation(z0))
@@ -303,7 +304,7 @@ x0 <- chainLast(fit$x)
 z0 <- chainLast(fit$z)
 
 model <- thresholdModel(twl$Twilight, twl$Rise, twilight.model = "LogNormal", 
-                        alpha = alpha, beta = beta, x0 = x0, z0 = z0, zenith = 98.6, 
+                        alpha = alpha, beta = beta, x0 = x0, z0 = z0, zenith = zenith, 
                         logp.x = log.prior, logp.z = log.prior,
                         fixedx = fixedx)
 
@@ -353,6 +354,88 @@ s <- locationSummary(fit$x,time=model$time,collapse=T)
 plot(wrld_simpl, col = "grey95", xlim = xlim, ylim = ylim)
 lines(s$Lon.mean, s$Lat.mean,col=rgb(t(col2rgb("firebrick"))/255,alpha=0.5))
 points(s$Lon.mean, s$Lat.mean,pch=16,cex=0.5,col=rgb(t(col2rgb("firebrick"))/255,alpha=0.2))
+
+
+# What if we know the birds are stationary from 1 December to 1 February?
+# It is possible to do a grouped analysis and specify that there is no movement 
+# during this period.
+
+stat <- which(twl$Twilight > as.POSIXct("2011-12-01 00:00:01", "GMT") & twl$Twilight < as.POSIXct("2012-02-01 00:00:01", "GMT"))
+min(stat) ; max(stat)
+grp <- c(1:(min(stat)-1), rep(min(stat),length(stat)), 
+         seq(from = min(stat)+1, by = 1, length.out = nrow(twl)-(max(stat))))
+
+#INITIALIZATION
+x0 <- path$x
+fixedx <- rep(F, nrow(x0))
+fixedx[1:2] <- T
+fixedx[nrow(x0) - 1] <- T
+fixedx[nrow(x0)] <- T
+x0[fixedx, 1] <- lon.calib
+x0[fixedx, 2] <- lat.calib
+z0 <- trackMidpts(x0) # update z0 positions
+
+
+
+#night only grouping
+grp2 <- rep(1:nrow(twl), each = 2, length.out = nrow(twl) + 1)[-1]
+#simplest grouping (no grouping at all)
+grp3 <- 1:658
+
+model <- groupedThresholdModel(twl$Twilight,twl$Rise, group = grp,
+                        twilight.model="ModifiedLogNormal",
+                        alpha=alpha,beta=beta,
+                        logp.x = log.prior, logp.z = log.prior, #mask is applied here
+                        x0=x0,z0=z0,zenith=zenith,fixedx=fixedx)
+
+proposal.x <- mvnorm(S=diag(c(0.005,0.005)),n=nlocation(x0))
+proposal.z <- mvnorm(S=diag(c(0.005,0.005)),n=nlocation(z0))
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# THIS FAILS!!!
+fit <- estelleMetropolis(model,proposal.x,proposal.z,iters=200,thin=20,chains=1)
+
+
+
+
+
+
+
+#TUNING
+
+x0 <- chainLast(fit$x)
+z0 <- chainLast(fit$z)
+
+model <- groupedThresholdModel(twl$Twilight,twl$Rise, group = grp,
+                               twilight.model="LogNormal",
+                               alpha=alpha,beta=beta,
+                               logp.x = log.prior, logp.z = log.prior, #mask is applied here
+                               x0=x0,z0=z0,zenith=zenith,fixedx=fixedx)
+
+proposal.x <- mvnorm(S=diag(c(0.005,0.005)),n=nlocation(x0))
+proposal.z <- mvnorm(S=diag(c(0.005,0.005)),n=nlocation(z0))
+
+fit <- estelleMetropolis(model,proposal.x,proposal.z,
+                         iters=300,thin=20,chains=2)
+
+for(k in 1:3) {
+  proposal.x <- mvnorm(chainCov(fit$x),s=0.2)
+  proposal.z <- mvnorm(chainCov(fit$z),s=0.2)
+  fit <- estelleMetropolis(model,proposal.x,proposal.z,
+                           x0=chainLast(fit$x),
+                           z0=chainLast(fit$z),
+                           iters=300,thin=20,chains=2)
+}
+
+#FINAL RUN
+
+proposal.x <- mvnorm(chainCov(fit$x),s=0.25)
+proposal.z <- mvnorm(chainCov(fit$z),s=0.25)
+
+fit <- estelleMetropolis(model,proposal.x,proposal.z,
+                         x0=chainLast(fit$x),
+                         z0=chainLast(fit$z),
+                         iters=2000,thin=20,chains=2)
 
 #Other possible model components and constraints?
 
