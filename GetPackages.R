5e8742d4e8b0d1a0a371bce39f81653669ff58f0
Mike Hallworth <mhallworth@gmail.com>
2016-08-09 13:02:24 -0400

    Added dependencies

--- GetPackages.R
+++ GetPackages.R
@@ -1,14 +1,14 @@
 # Check to make sure the required packages are installed on your machine
 # If not, they will be installed
 
-reqPackages <- c("devtools","GeoLight","rages","geosphere","raster","fields","forecast",
+reqPackages <- c("devtools","digest","GeoLight","rages","geosphere","raster","fields","forecast",
                   "circular","truncnom","parallel","bit","rgdal","CircStats","Rcpp","RcppArmadillo",
 				  "ggmap","ggsn","sp","maptools","rgeos","MASS")
 
 
 get.packages <- reqPackages[!(reqPackages %in% installed.packages()[,"Package"])]
 
-if(length(get.packages)>0) install.packages(get.packages,repos = "https://cloud.r-project.org/")
+if(length(get.packages)>0) install.packages(get.packages,repos = "https://cloud.r-project.org/",dependencies=TRUE)
 
 # Install necessary packages from Github using the devtools library #
 
